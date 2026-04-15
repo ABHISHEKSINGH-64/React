6a223a109dff47f9d1929d9233a7e622ccdc5889
Abhishek Singh <abhisherajput64@gmail.com>
2026-04-15 19:25:37 +0530

    push

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,31 +1,41 @@
 import React, { useState } from "react";
 
-function Counter() {
-  const [count, setCount] = useState(0);
+// function Counter() {
+//   const [count, setCount] = useState(0);
 
-  return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Count: {count}</h1>
+//   return (
+//     <div style={{ textAlign: "center", marginTop: "50px" }}>
+//       <h1>Count: {count}</h1>
 
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+//       <button onClick={() => setCount(count + 1)}>Increment</button>
 
-      <button
-        onClick={() => setCount(count - 1)}
-        style={{ margin: "0 10px" }}
-      >
-        Decrement
-      </button>
+//       <button onClick={() => setCount(count - 1)}style={{ margin: "0 10px" }} > Decrement </button>
 
-      <button onClick={() => setCount(0)}>Reset</button>
-    </div>
+//       <button onClick={() => setCount(0)}>Reset</button>
+//     </div>
+//   );
+// }
+
+
+
+function Counter(){
+  const [currentColor, setColor] = useState("green")
+  return(
+    <>
+      <div>
+        <div>
+          <button onClick={() => setColor("blue")}>red</button>
+          <button onClick={() => setColor("green")}>green</button>
+           <button onClick={() => setColor("blue")}>blue</button>
+        </div>
+      </div>
+    </>
   );
 }
+
 
 export default Counter;
 
 
 
 
-  // const increment = () => setCount(prev => prev + 1);
-  // const decrement = () => setCount(prev => prev - 1);
-  // const reset = () => setCount(0);
