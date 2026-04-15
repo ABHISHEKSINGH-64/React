16723060d48efc556ae91b0dc235ec0f0b55d568
Abhishek Singh <abhisherajput64@gmail.com>
2026-04-15 16:11:33 +0530

    update

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,28 +1,31 @@
 import React, { useState } from "react";
 
 function Counter() {
-  
   const [count, setCount] = useState(0);
 
-  
-  const increment = () => setCount(prev => prev + 1);
-  const decrement = () => setCount(prev => prev - 1);
-  const reset = () => setCount(0);
-
   return (
-   <>
-        <div style={{ textAlign: "center", marginTop: "50px" }}>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Count: {count}</h1>
 
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement} style={{ margin: "0 10px" }}>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+
+      <button
+        onClick={() => setCount(count - 1)}
+        style={{ margin: "0 10px" }}
+      >
         Decrement
       </button>
-      <button onClick={reset}>Reset</button>
+
+      <button onClick={() => setCount(0)}>Reset</button>
     </div>
-   </>
   );
 }
 
 export default Counter;
 
+
+
+
+  // const increment = () => setCount(prev => prev + 1);
+  // const decrement = () => setCount(prev => prev - 1);
+  // const reset = () => setCount(0);
